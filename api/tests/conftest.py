b8290d1554b8bc1b0087db34f2f312e3b09d737d
50601 <506017129@qq.com>
2025-10-15 17:45:17 +0100

    black clean code standard

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
+
 from api.app.core.db import Base, get_db
 from api.app.main import app
 
