1d18e55ec84b0f78fec77636fa40afaf1f1dd4ef
50601 <506017129@qq.com>
2025-11-09 17:30:06 +0000

    issue #3

--- web/src/layouts/MainLayout.tsx
+++ web/src/layouts/MainLayout.tsx
@@ -1,23 +1,22 @@
 import Navbar from "../component/Navbar.tsx";
 import Background from "../component/Background.tsx";
-import {useLocation} from "react-router-dom";
 
 export default function MainLayout({children}: { children: React.ReactNode }) {
-    const location = useLocation();
+    // const location = useLocation();
+    //
+    // const backgrounds: Record<string, string> = {
+    //     "/": "/images/goalbet_logo_black_bg.png",
+    //     "/about": "/images/goalbet_logo_black_bg.png",
+    //     "/goals": "/images/goalbet_logo_black_bg.png",
+    //     "/wallet": "/images/goalbet_logo_black_bg.png",
+    //     "/login": "/images/goalbet_logo_black_bg.png",
+    // };
 
-    const backgrounds: Record<string, string> = {
-        "/": "/images/goalbet_logo_black_bg.png",
-        "/about": "/images/goalbet_logo_black_bg.png",
-        "/goals": "/images/goalbet_logo_black_bg.png",
-        "/wallet": "/images/goalbet_logo_black_bg.png",
-        "/login": "/images/goalbet_logo_black_bg.png",
-    };
-
-    const currentBg = backgrounds[location.pathname] || "/images/goalbet_logo_black_bg.png";
+    // const currentBg = backgrounds[location.pathname] || "/images/goalbet_logo_black_bg.png";
 
     return (
         <div className="min-h-screen text-white relative overflow-hidden">
-            <Background image={currentBg}/>
+            <Background/>
             <header className={"relative z-10"}>
                 <Navbar/>
             </header>
