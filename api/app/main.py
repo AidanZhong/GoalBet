e0b52ccbd35239a02f8d9e24003d3f1c5a2b4446
50601 <506017129@qq.com>
2025-11-12 00:24:57 +0000

    aws db creation

--- api/app/main.py
+++ api/app/main.py
@@ -102,6 +102,7 @@ async def log_exceptions(request: Request, call_next):
 
 @app.on_event("startup")
 async def on_startup() -> None:
+    Base.metadata.create_all(bind=engine)
     try:
         url = str(engine.url)
     except Exception:
